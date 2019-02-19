c1697a215a29cc133e75a74683b13f39f5420de6
AHMET UYSAL <AUYSAL16@ku.edu.tr>
2019-02-19 22:33:46 +0300

    Added links for review0

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -22,7 +22,8 @@ const ASSIGNMENT_DATA: Assignment[] = [
     no: 0,
     title: 'Welcome to Python',
     handoutLink: 'https://github.com/koltpython/python-assignments/blob/master/Assignment0/Assignment0.md',
-    reviewLink: null,
+    reviewCondensed: 'https://github.com/koltpython/python-slides/raw/master/Review0/review0-condensed.pdf',
+    reviewFull: 'https://github.com/koltpython/python-slides/raw/master/Review0/review0.pdf',
     starterCode: null,
     releaseDate: new Date(2019, 1, 18, 19, 0),
     dueDate: new Date(2019, 1, 25, 17, 30)  
@@ -33,7 +34,8 @@ export interface Assignment {
   no: number,
   title: String,
   handoutLink: String,
-  reviewLink: String,
+  reviewCondensed: String,
+  reviewFull: String,
   starterCode: String,
   releaseDate: Date,
   dueDate: Date
