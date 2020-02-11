69be53883ccbc951b42f1b7fe7379eedba07efa4
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-12 02:23:59 +0300

    Add section2 links

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -16,12 +16,21 @@ export class SectionComponent implements OnInit {
 const SECTION_DATA: Section[] = [
   {
     topic: 'Variables, Console I/O & Branching',
-    date: new Date(2020, 1, 4),
+    date: new Date(2020, 1, 5),
     handout: 'https://github.com/koltpython/python-exercises-spring2020/raw/master/section1/section1.pdf',
     starter:
       'https://kinolien.github.io/gitzip/?download=koltpython/python-exercises-spring2020/tree/master/section1/starter',
     solution:
       'https://kinolien.github.io/gitzip/?download=koltpython/python-exercises-spring2020/tree/master/section1/solution'
+  },
+  {
+    topic: 'While Loops, Turtle Graphics & Strings',
+    date: new Date(2020, 1, 12),
+    handout: 'https://github.com/koltpython/python-exercises-spring2020/raw/master/section2/section2.pdf',
+    starter:
+      'https://kinolien.github.io/gitzip/?download=koltpython/python-exercises-spring2020/tree/master/section2/starter',
+    solution:
+      'https://kinolien.github.io/gitzip/?download=koltpython/python-exercises-spring2020/tree/master/section2/solution'
   }
 ];
 
